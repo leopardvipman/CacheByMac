25bc6ad19bc2f5d9c62b3fdd210368c349dd3810
hongqiangyang <leopardvipman@gmail.com>
2013-02-21 10:58:09 +0800

    Initial Commit

---
+++ CacheByMac/CAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  CAppDelegate.h
+//  CacheByMac
+//
+//  Created by 杨 宏强 on 13-2-21.
+//  Copyright (c) 2013年 yanghongqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class CViewController;
+
+@interface CAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) CViewController *viewController;
+
+@end
---
+++ CacheByMac/CViewController.h
@@ -0,0 +1,13 @@
+//
+//  CViewController.h
+//  CacheByMac
+//
+//  Created by 杨 宏强 on 13-2-21.
+//  Copyright (c) 2013年 yanghongqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CViewController : UIViewController
+
+@end
